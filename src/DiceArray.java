ccfdef98962587147340c01ba7c8b3cd4a9ae541
jh1923 <42978576+jh1923@users.noreply.github.com>
2018-10-28 17:30:46 -0400

    Update DiceArray.java

--- src/DiceArray.java
+++ src/DiceArray.java
@@ -1,11 +1,16 @@
-
+/**
+ * 
+ * @author Julia Hu
+ * DiceArray main
+ *
+ */
 public class DiceArray 
 {
 
 	public static void main(String[] args)
 	{
 		//array
-		int [] values = new int[13]
+		int [] values = new int[13];
 		
 		//creates two dice objects
 		Dice dice1 = new Dice();
@@ -15,13 +20,19 @@ public class DiceArray
 		int roll1;
 		int roll2;
 		
-		for (int i=0, i<1000, i++)
+		//rolls two dice, sums them, and adds 1 for each instance of given sum
+		for (int i=0; i<1000; i++)
 		{
 			roll1 = dice1.rollDice();
 			roll2 = dice2.rollDice();
 			int sum = roll1 + roll2;
+			values [sum]++;
+		}
 		
-			
+		//displays number and corresponding number of rolls
+		for (int i=2; i<13; i++)
+		{
+			System.out.println("You rolled " + i + " " +  values[i] + " times.");
 		}
 
 	}
